887fdb2c468d9d9ac72f327e27811f5a6caeccb2
fabio-martin <fabiomartin2006@gmail.com>
2022-08-02 14:45:52 +0100

    End of Section 5

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 
-
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   // <React.StrictMode>
-    <App />
+  <App />
   // </React.StrictMode>
 );
 
